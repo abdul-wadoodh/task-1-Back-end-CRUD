c3fa6167c570b186885e8a4b1f3b5dcfae1cd5e0
abdul wadoodh <abdul@provility.com>
2023-07-15 23:57:00 +0530

    checking git

--- src/main/java/com/crudtask/Task/TaskApplication.java
+++ src/main/java/com/crudtask/Task/TaskApplication.java
@@ -3,7 +3,6 @@ package com.crudtask.Task;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//checking git
 @SpringBootApplication
 public class TaskApplication {
 
